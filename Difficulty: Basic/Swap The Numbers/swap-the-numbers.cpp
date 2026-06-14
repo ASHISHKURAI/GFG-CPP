7f9f2b6abc7339870a87f25721ece23d99c969e9
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-14 22:53:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+void swap(int &a, int &b) {
+    // code here
+    int c;
+    c=a;
+    a=b;
+    b=c;
+}
+//int main()
+//{
+ //   int a,b;
+ /*   cin>>a>>b;
+    swap(a,b);
+    cout <<a<<b;
+} */
