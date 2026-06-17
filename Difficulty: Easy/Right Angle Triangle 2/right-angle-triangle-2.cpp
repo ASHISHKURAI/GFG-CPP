0d07f7a4168029cedc2880763cb03e06cbdf9598
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-17 22:14:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right Angle Triangle 2/right-angle-triangle-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void printPattern(int n) {
+        // Code here
+        for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if((i==j) || (j==0) || (i==(n-1)))
+              cout<<"* ";
+            else
+              cout<<"  ";
+        }
+        cout<<"\n";
+      }
+      return;
+    }
+};
