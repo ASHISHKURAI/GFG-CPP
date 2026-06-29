0a21ad85cbb342f2e6ea771004f9a4cffe17981e
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-29 22:13:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Dot Product with 0 Insertions/max-dot-product-with-0-insertions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    
+    int maxDotProduct(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+        vector<int>dp(m+1,0);
+        
+        for(int i=n-1;i>=0;i--){
+            int next = 0;
+            for(int j=m-1;j>=0;j--){
+               
+               int temp2 = dp[j];
+               int temp = 0;
+               
+               if(n-i!=m-j) temp = dp[j];
+               dp[j] = max(temp,(a[i]*b[j]) + next); 
+               next = temp2;
+            }
+        }
+        
+        return dp[0];
+    }
+};
